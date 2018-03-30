fadc4e94886ec37a7dd64c5300606a371d4e2269
spanfil <spanfil@itti.com.pl>
2018-03-30 12:14:24 +0200

    TrialSession sample test

---
+++ api/src/main/java/pl/com/itti/app/driver/repository/TrialSessionRepository.java
@@ -0,0 +1,9 @@
+package pl.com.itti.app.driver.repository;
+
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import pl.com.itti.app.driver.model.TrialSession;
+
+public interface TrialSessionRepository extends PagingAndSortingRepository<TrialSession, Long>, JpaSpecificationExecutor {
+
+}
