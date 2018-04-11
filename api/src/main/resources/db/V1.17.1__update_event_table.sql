841cb8e75860e88d87cc65121051fa1c8d5e753b
aplonder <aplonder@itti.com.pl>
2018-04-11 13:04:07 +0200

    with_data_update

---
+++ api/src/main/resources/db/V1.17.1__update_event_table.sql
@@ -0,0 +1,4 @@
+UPDATE public.event SET "trial_user_id" = '1', "trial_role_id" = '1' WHERE "id" = '1';
+UPDATE public.event SET "trial_user_id" = '2', "trial_role_id" = '2' WHERE "id" = '3';
+UPDATE public.event SET "trial_user_id" = '3', "trial_role_id" = '6' WHERE "id" = '4';
+UPDATE public.event SET "trial_user_id" = '4', "trial_role_id" = '6' WHERE "id" = '6';
