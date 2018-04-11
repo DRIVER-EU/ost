dd02b1143c955a55df30347783088f00c8bb153e
aplonder <aplonder@itti.com.pl>
2018-04-11 16:15:59 +0200

    done

--- api/src/main/java/pl/com/itti/app/driver/dto/EventDTO.java
+++ api/src/main/java/pl/com/itti/app/driver/dto/EventDTO.java
@@ -2,8 +2,11 @@ package pl.com.itti.app.driver.dto;
 
 import co.perpixel.dto.EntityDTO;
 import pl.com.itti.app.driver.model.Event;
+import pl.com.itti.app.driver.model.TrialRole;
+import pl.com.itti.app.driver.model.TrialUser;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class EventDTO {
 
@@ -24,12 +27,26 @@ public class EventDTO {
         public String name;
         public String description;
         public LocalDateTime eventTime;
+        public long trialUserId;
+        public String trialUserFirstName;
+        public String trialUserLastName;
+        public long trialRoleId;
+        public String trialRoleName;
 
         public ListItem(Event event) {
             super(event);
             this.name = event.getName();
             this.description = event.getDescription();
             this.eventTime = event.getEventTime();
+
+            Optional<TrialUser> trialUserOptional = Optional.ofNullable(event.getTrialUser());
+            this.trialUserId = trialUserOptional.map(TrialUser::getId).orElse(0L);
+            this.trialUserFirstName = trialUserOptional.map(t -> t.getAuthUser().getFirstName()).orElse("");
+            this.trialUserLastName = trialUserOptional.map(t -> t.getAuthUser().getLastName()).orElse("");
+
+            Optional<TrialRole> trialRoleOptional = Optional.ofNullable(event.getTrialRole());
+            this.trialRoleId = trialRoleOptional.map(TrialRole::getId).orElse(0L);
+            this.trialRoleName = trialRoleOptional.map(TrialRole::getName).orElse("");
         }
     }
 }
--- api/src/main/java/pl/com/itti/app/driver/repository/EventRepository.java
+++ api/src/main/java/pl/com/itti/app/driver/repository/EventRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import pl.com.itti.app.driver.model.Event;
 
-import java.util.List;
-
 public interface EventRepository extends PagingAndSortingRepository<Event, Long>, JpaSpecificationExecutor {
     Page<Event> findByTrialSessionId(long trialSessionId, Pageable pageable);
 }
