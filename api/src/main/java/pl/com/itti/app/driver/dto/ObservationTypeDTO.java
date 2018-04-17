0af5c9dd059a0a01821be6a6032ea158611c6a5b
bszymanski <bszymanski@itti.com.pl>
2018-04-17 16:15:38 +0200

    CR fixes

--- api/src/main/java/pl/com/itti/app/driver/dto/ObservationTypeDTO.java
+++ api/src/main/java/pl/com/itti/app/driver/dto/ObservationTypeDTO.java
@@ -1,11 +1,12 @@
-package pl.com.itti.app.driver.web.dto;
+package pl.com.itti.app.driver.dto;
 
 import co.perpixel.dto.EntityDTO;
 import pl.com.itti.app.driver.model.ObservationType;
 
-public class ObservationTypeDTO {
+public final class ObservationTypeDTO {
 
     public static class MinimalItem implements EntityDTO<ObservationType> {
+
         public long id;
 
         @Override
@@ -15,15 +16,19 @@ public class ObservationTypeDTO {
     }
 
     public static class ListItem extends ObservationTypeDTO.MinimalItem {
+
         public String name;
         public String description;
 
         @Override
         public void toDto(ObservationType observationType) {
             super.toDto(observationType);
-
             this.name = observationType.getName();
             this.description = observationType.getDescription();
         }
+    }
+
+    private ObservationTypeDTO() {
+        throw new AssertionError();
     }
 }
--- api/src/main/java/pl/com/itti/app/driver/web/ObservationTypeController.java
+++ api/src/main/java/pl/com/itti/app/driver/web/ObservationTypeController.java
@@ -1,4 +1,4 @@
-package pl.com.itti.app.driver.web.rest;
+package pl.com.itti.app.driver.web;
 
 import co.perpixel.dto.DTO;
 import co.perpixel.dto.PageDTO;
@@ -8,18 +8,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pl.com.itti.app.driver.dto.ObservationTypeDTO;
 import pl.com.itti.app.driver.service.ObservationTypeService;
-import pl.com.itti.app.driver.web.dto.ObservationTypeDTO;
 
 @RestController
-@RequestMapping("/api/observation_types")
+@RequestMapping("/api/observationtypes")
 public class ObservationTypeController {
 
     @Autowired
     private ObservationTypeService observationTypeService;
 
     @GetMapping
-    public PageDTO<ObservationTypeDTO.ListItem> findAll(@RequestParam("trialSessionId") Long trialSessionId, Pageable pageable) {
+    public PageDTO<ObservationTypeDTO.ListItem> findAll(@RequestParam("trialsession_id") Long trialSessionId, Pageable pageable) {
         return DTO.from(observationTypeService.find(trialSessionId, pageable), ObservationTypeDTO.ListItem.class);
     }
 }
