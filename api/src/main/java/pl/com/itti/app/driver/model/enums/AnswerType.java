8cd587c9383603e33aae26597070c62b778574b1
bszymanski <bszymanski@itti.com.pl>
2018-04-18 10:11:13 +0200

    Revert: Init schema creator (4da9223)

--- api/src/main/java/pl/com/itti/app/driver/model/enums/AnswerType.java
+++ api/src/main/java/pl/com/itti/app/driver/model/enums/AnswerType.java
@@ -1,18 +1,8 @@
 package pl.com.itti.app.driver.model.enums;
 
 public enum AnswerType {
-    CHECKBOX("boolean"),
-    RADIO_BUTTON("boolean"),
-    SLIDER("integer"),
-    TEXT_FIELD("string");
-
-    private String type;
-
-    AnswerType(String type) {
-        this.type = type;
-    }
-
-    public String getType() {
-        return type;
-    }
+    CHECKBOX,
+    RADIO_BUTTON,
+    SLIDER,
+    TEXT_FIELD
 }
