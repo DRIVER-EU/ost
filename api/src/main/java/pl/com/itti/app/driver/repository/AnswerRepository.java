32d28aa8db1150a71f5c826269828d8b44d79196
bszymanski <bszymanski@itti.com.pl>
2018-04-23 15:48:50 +0200

    Add filters to event and answers

--- api/src/main/java/pl/com/itti/app/driver/repository/AnswerRepository.java
+++ api/src/main/java/pl/com/itti/app/driver/repository/AnswerRepository.java
@@ -5,11 +5,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import pl.com.itti.app.driver.model.Answer;
 
-import java.util.List;
-
 @Repository
 public interface AnswerRepository
         extends PagingAndSortingRepository<Answer, Long>, JpaSpecificationExecutor<Answer> {
-
-    List<Answer> findAllByTrialSessionId(long trialSessionId);
 }
---
+++ api/src/main/java/pl/com/itti/app/driver/repository/specification/AnswerSpecification.java
@@ -0,0 +1,36 @@
+package pl.com.itti.app.driver.repository.specification;
+
+import co.perpixel.security.model.AuthUser;
+import org.springframework.data.jpa.domain.Specification;
+import pl.com.itti.app.driver.model.*;
+import pl.com.itti.app.driver.util.RepositoryUtils;
+
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
+
+public class AnswerSpecification {
+
+    public static Specification<Answer> isConnectedToAuthUser(AuthUser authUser) {
+        if (authUser == null) {
+            return null;
+        }
+
+        return (root, query, cb) -> {
+            Join<Answer, TrialUser> trialUserJoin = RepositoryUtils.getOrCreateJoin(root, Answer_.trialUser, JoinType.LEFT);
+
+            return cb.equal(trialUserJoin.get(TrialUser_.authUser), authUser);
+        };
+    }
+
+    public static Specification<Answer> inTrialSession(Long trialSessionId) {
+        if (trialSessionId == null) {
+            return null;
+        }
+
+        return (root, query, cb) -> {
+            Join<Answer, TrialSession> trialSessionJoin = RepositoryUtils.getOrCreateJoin(root, Answer_.trialSession, JoinType.LEFT);
+
+            return cb.equal(trialSessionJoin.get(TrialSession_.id), trialSessionId);
+        };
+    }
+}
