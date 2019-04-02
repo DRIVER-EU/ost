39fc2f19e31eec4b6fce56b8b29a16c3d1e9f380
Łukasz Pulik <lukasz.pulik@itti.com.pl>
2019-04-02 14:06:33 +0200

    fix

--- api/src/main/java/pl/com/itti/app/driver/util/SimulationTime.java
+++ api/src/main/java/pl/com/itti/app/driver/util/SimulationTime.java
@@ -2,7 +2,10 @@ package pl.com.itti.app.driver.util;
 
 import eu.driver.adapter.constants.TopicConstants;
 import eu.driver.adapter.core.CISAdapter;
-import eu.driver.examples.adapter.PrintAdapterCallback;
+import eu.driver.api.IAdaptorCallback;
+import eu.driver.model.core.Timing;
+import org.apache.avro.generic.IndexedRecord;
+import org.apache.avro.specific.SpecificData;
 import org.springframework.stereotype.Component;
 
 import java.time.*;
@@ -10,19 +13,44 @@ import java.time.*;
 @Component
 public class SimulationTime {
 
+    private static Timing timing = null;
+
     public static CISAdapter adapterInit(){
+        CISAdapter.globalConfigPath = "/opt/config/";
         CISAdapter adapter = CISAdapter.getInstance();
-        adapter.addCallback(new PrintAdapterCallback(), TopicConstants.TIMING_TOPIC);
+        adapter.addCallback(new CallbackValue(), TopicConstants.TIMING_TOPIC);
         return adapter;
     }
 
     public static LocalDateTime getTrialTime(){
-        return adapterInit().getTrialTime().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        CISAdapter cisAdapter = adapterInit();
+        if (timing != null) {
+            return Instant.ofEpochMilli(timing.getTrialTime()).atZone(ZoneId.systemDefault()).toLocalDateTime();
+        }
+        return cisAdapter.getTrialTime().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
     }
 
     public static LocalTime getTimeElapsed(){
-        return LocalDateTime.ofInstant(Instant.ofEpochMilli(adapterInit().getTimeElapsed()), ZoneId.systemDefault()).toLocalTime();
+        CISAdapter cisAdapter = adapterInit();
+        if (timing != null) {
+            return Instant.ofEpochMilli(timing.getTimeElapsed()).atZone(ZoneId.systemDefault()).toLocalTime();
+        }
+        return LocalDateTime.ofInstant(Instant.ofEpochMilli(cisAdapter.getTimeElapsed()), ZoneId.systemDefault()).toLocalTime();
     }
 
+    public static class CallbackValue implements IAdaptorCallback {
+        public CallbackValue() {
+        }
+
+        public void messageReceived(IndexedRecord key, IndexedRecord receivedMessage, String topicName) {
+            if (receivedMessage.getSchema().getName().equalsIgnoreCase("Timing")) {
+                try {
+                    timing = (eu.driver.model.core.Timing) SpecificData.get().deepCopy(eu.driver.model.core.Timing.SCHEMA$, receivedMessage);
+                } catch (Exception e) {
+                    System.out.println("Error Timing receive message! " + e.getMessage());
+                }
+            }
+        }
+    }
 
 }
