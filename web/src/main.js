211e6c4c59587555c146402dffde691dd8549990
Tomek Filipiak <tfilipiak@itti.com.pl>
2019-08-01 19:45:54 +0200

    fix :D

--- web/src/main.js
+++ web/src/main.js
@@ -87,7 +87,7 @@ const store = createStore(initialState)
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('./store/cashSite.js')
+      .register('/cashSite.js')
       .then(reg => console.log('Service Worker: Registered (Pages)'))
       .catch(err => console.log(`Service Worker: Error: ${err}`))
   })
