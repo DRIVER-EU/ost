1b3632917f891deff2cf14ee3e2f0dd3321ce74f
rorczyk <robert.orczyk@itti.com.pl>
2019-10-15 11:49:47 +0200

    - rest version

--- api/src/main/java/pl/com/itti/app/driver/web/TrialUserController.java
+++ api/src/main/java/pl/com/itti/app/driver/web/TrialUserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pl.com.itti.app.Application;
 import pl.com.itti.app.driver.dto.TrialUserDTO;
 import pl.com.itti.app.driver.service.TrialUserService;
 
@@ -22,5 +23,10 @@ public class TrialUserController {
     private PageDTO<TrialUserDTO.ListItem> findByTrialSessionId(
             @RequestParam(value = "trialsession_id") long trialSessionId, Pageable pageable) {
         return DTO.from(trialUserService.findByTrialSessionId(trialSessionId, pageable), TrialUserDTO.ListItem.class);
+    }
+
+    @GetMapping("/version")
+    public String getVersion() {
+        return Application.version;
     }
 }
