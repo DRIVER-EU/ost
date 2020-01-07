6737a21d355c437d5508d91cc12ae9a2be083f07
asoft <wiktorowski@a-soft.pl>
2020-01-07 15:49:23 +0100

    asoft 350-351-353

--- web/src/config/Api.js
+++ web/src/config/Api.js
@@ -3,5 +3,5 @@ export let origin = window.location.origin
 if (origin.includes('localhost') || origin.includes('dev.itti.com.pl')) {
   origin = 'https://testbed-ost.itti.com.pl'
 } else {
-  origin = 'http://window.location.host'
+  origin = 'window.location.host'
 }
