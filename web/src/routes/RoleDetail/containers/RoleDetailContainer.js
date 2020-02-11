c918246636e6f73bdfe07f29e6b4477419f4207d
Mikołaj Kaczmarczyk <m.kaczmarczyk@a-soft.pl>
2020-02-11 11:55:52 +0000

    A soft bugixing

--- web/src/routes/RoleDetail/containers/RoleDetailContainer.js
+++ web/src/routes/RoleDetail/containers/RoleDetailContainer.js
@@ -1,5 +1,8 @@
 import { connect } from 'react-redux'
 import { getTrialDetail } from '../../TrialDetail/modules/trialdetail'
+import { getUsersList,
+  addUser,
+  removeUser } from '../../SessionDetail/modules/sessiondetail'
 import RoleDetailView from '../components/RoleDetail'
 import { getRoleById, removeRole, updateRole } from '../modules/roledetail'
 
@@ -7,16 +10,23 @@ const mapDispatchToProps = {
   getTrialDetail,
   getRoleById,
   removeRole,
-  updateRole
+  updateRole,
+  getUsersList,
+  addUser,
+  removeUser
 }
 
 const mapStateToProps = state => {
   return {
     trialName: state.trialDetail.trialName,
+    roleSet: state.trialDetail.roleSet,
     roleId: state.roleDetail.id,
     roleName: state.roleDetail.roleName,
     roleType: state.roleDetail.roleType,
-    questions: state.roleDetail.questions
+    questions: state.roleDetail.questions,
+    userRoles: state.roleDetail.userRoles,
+    usersList: state.sessionDetail.usersList
+
   }
 }
 
