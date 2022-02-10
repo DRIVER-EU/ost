f243e39bd22cbae19545f33978b775a2b48c71ae
bfabisiak <bartosz.fabisiak@itti.com.pl>
2022-02-10 16:07:26 +0100

    Fixed new user creation. Added LICENSE. Parametrized database configuration.

--- web/src/components/Users/components/Users.js
+++ web/src/components/Users/components/Users.js
@@ -255,7 +255,7 @@ class Users extends Component {
         this.props.putUserPassword(userId, { password: user.password })
       }
     } else {
-      const role = user.isAdmin ? 'ost_admin' : 'ost_observer'
+      const _rolesIds = user.isAdmin ? '1' : '2'
       const updatedUser = {
         login: user.login,
         password: user.password,
@@ -264,7 +264,7 @@ class Users extends Component {
         email: user.email,
         contact: user.contact,
         activated: user.activated,
-        roles: [role]
+        rolesIds: [_rolesIds]
       }
       this.props.addUser(updatedUser)
     }
